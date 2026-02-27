50907759b2ca0a3353672a401361a54dd2fc6a01
Hemanth <kasaramhemanth3@gmail.com>
2026-02-27 22:33:34 +0530

    Fixed locals currentUser null safety

--- app.js
+++ app.js
@@ -82,12 +82,11 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
+    res.locals.currentUser = req.user || null;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
-
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
